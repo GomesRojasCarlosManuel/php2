eedc80eb87da10b22e20ceae7e49e5b21b058af2
Carlos Gomes <gomezrojascarlosmanuel@gmail.com>
2024-11-19 19:12:49 -0500

    se añadio ejercicio01.php

---
+++ ejercicios/ejercicio01.php
@@ -0,0 +1,8 @@
+<?php
+    $sueldo=1000;
+    // Cálculo del descuento del 9%
+    $descuento = $sueldo * 0.09;
+
+    // Mostrar el resultado con dos decimales usando printf
+    printf("El descuento de EsSalud es: S/ %.2f", $descuento);
+?>
