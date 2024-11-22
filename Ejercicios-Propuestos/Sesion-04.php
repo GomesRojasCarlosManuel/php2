6ba690aed5b7acd7e1170170cc8c5acf5156e67e
Carlos Gomes <gomezrojascarlosmanuel@gmail.com>
2024-11-21 19:17:15 -0500

    Actualizado

---
+++ Ejercicios-Propuestos/Sesion-04.php
@@ -0,0 +1,33 @@
+<?php
+// Definimos una cadena de texto
+$cadena = "Aprender PHP es muy interesante y divertido.";
+
+// 1. Longitud de la cadena
+$longitud = strlen($cadena);
+echo "1. La longitud de la cadena es: $longitud caracteres.<br>";
+
+// 2. Buscar una subcadena
+$subcadena = "PHP";
+$posicion = strpos($cadena, $subcadena); // Busca la posición de la subcadena
+if ($posicion !== false) {
+    echo "2. La subcadena '$subcadena' se encontró en la posición: $posicion.<br>";
+} else {
+    echo "2. La subcadena '$subcadena' no se encontró.<br>";
+}
+
+// 3. Extraer una parte de la cadena con substr
+$inicio = 10; // Desde la posición 10
+$longitud_extraer = 10; // Extraemos 10 caracteres
+$cadena_extraida = substr($cadena, $inicio, $longitud_extraer);
+echo "3. La parte extraída de la cadena es: '$cadena_extraida'.<br>";
+
+// 4. Convertir toda la cadena a minúsculas con strtolower
+$cadena_minusculas = strtolower($cadena);
+echo "4. La cadena en minúsculas es: '$cadena_minusculas'.<br>";
+
+// 5. Reemplazar palabras en la cadena con str_replace
+$palabra_original = "interesante";
+$palabra_nueva = "fácil";
+$cadena_reemplazada = str_replace($palabra_original, $palabra_nueva, $cadena);
+echo "5. La cadena después del reemplazo es: '$cadena_reemplazada'.<br>";
+?>
--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 # los laboratorios se veran asi en la carpeta "Laboratorios"
 
 lab01
+
 lab02
+
 lab03
+
 ...
 
 tambien cada laboratorio llevara un "README" que contendra su Numero de la SESION del LABORATORIO
 
+
 # los Ejercicios propuestos se veran asi"
 
 Sesion-02
+
 Sesion-03
+
 Sesion-04
+
 ... 
 
 cada "Sesion-X" llevara su respectivo ejercicio 
